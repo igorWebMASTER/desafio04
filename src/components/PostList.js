49f2c46aed7c4ca56a0cb9c5e08d82edc43d2a5d
igorWebMASTER <igor_1917@hotmail.com>
2020-02-22 23:48:07 -0300

    PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -24,25 +24,6 @@ class PostList extends Component {
           }
         ]
       },
-      id: 2
-      {
-      author: {
-        name: "Igor Ribeiro",
-        avatar: "https://avatars2.githubusercontent.com/u/8940529?s=460&v=4"
-      },
-      date: "04 Jun 2019",
-      content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
-      comments: [
-        {
-          id: 1,
-          author: {
-            name: "Diego Fernandes",
-            avatar: "https://avatars2.githubusercontent.com/u/8940529?s=460&v=4"
-          },
-          content: "Conteúdo do comentário"
-        }
-      ]
-    }
     ]
   };
 
